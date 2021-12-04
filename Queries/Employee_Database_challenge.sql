b0f088f500418cf204e33330b3bb3c709964b903
utpetroleum <quang.nguyen@utexas.edu>
2021-12-04 15:30:02 -0600

    commits

---
+++ Queries/Employee_Database_challenge.sql
@@ -0,0 +1,53 @@
+-- Deliverable 1: The Number of Retiring Employees by Title
+-- Create retirement_titles table
+SELECT e.emp_no, e.first_name, e.last_name, t.title, t.from_date, t.to_date
+INTO retirement_titles
+FROM employees AS e
+INNER JOIN titles AS t
+ON e.emp_no = t.emp_no
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+ORDER BY e.emp_no ASC;
+
+SELECT * FROM retirement_titles;
+
+DROP TABLE retirement_titles;
+
+-- Use Dictinct with Orderby to remove duplicate rows
+SELECT DISTINCT ON (emp_no) emp_no,
+first_name,
+last_name,
+title
+INTO unique_titles
+FROM retirement_titles
+ORDER BY emp_no, to_date DESC;
+
+--Write another query to retrieve the number of employees by their most recent job title who are about to retire.
+SELECT COUNT(title) AS count, title
+INTO retiring_titles
+FROM unique_titles
+GROUP BY title
+ORDER BY count DESC;
+
+SELECT * FROM retiring_titles;
+
+-- Deliverable 2: The Employees Eligible for the Mentorship Program
+
+SELECT DISTINCT ON (e.emp_no) e.emp_no, e.first_name, e.last_name, e.birth_date, de.from_date, de.to_date, t.title
+INTO mentorship_eligibilty
+FROM employees AS e
+INNER JOIN dept_emp AS de
+ON e.emp_no = de.emp_no
+INNER JOIN titles AS t
+ON e.emp_no = t.emp_no
+WHERE (de.to_date = '9999-01-01') 
+AND (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY e.emp_no;
+
+SELECT * FROM mentorship_eligibilty;
+
+SELECT COUNT(title) AS count, title
+FROM mentorship_eligibilty
+GROUP BY title
+ORDER BY count DESC;
+
+
--- README.md
+++ README.md
@@ -1,1 +1,31 @@
 # Pewlett-Hackard-Analysis
+
+## Overview of Project
+
+### Purpose
+The purpose of this project is to determine the number of retiring employees per title, and identify employees who are eligible to participate in a mentorship program.
+
+## Results
+
+### The Number of Retiring Employees by Title
+
+![Retiring Employees by Title](https://user-images.githubusercontent.com/92401000/144724937-0accc64b-7943-4f06-adc9-79d9caf785bb.PNG)
+
+- The most amount of employees retiring will be from "Senior Engineer" and "Senior Staff". 29,414 "Senior Engineer" and 28,254 "Senior Staff" is of retirement age.
+
+- The least amount of employees retiring will be from "Assistant Engineer" and "Manager". 1,761 "Assistant Enginner" and 2 "Manager" is of retirement age. 
+
+### The Employees Eligible for the Mentorship Program
+
+![Employees Eligible for the Mentorship Program](https://user-images.githubusercontent.com/92401000/144724942-ca8c1e78-758d-43df-9305-87bcb86b6d81.PNG)
+
+- Throughout the entire company of Pewlett Hackard, only 1,549 current employees are eligible to participate in a mentorship program. 
+
+- There are zero employees under the "Manager" tile eligible to participate in a mentorship program. 
+
+## Summary
+
+### How many roles will need to be filled as the "silver tsunami" begins to make an impact?
+
+### Are there enough qualified, retirement-ready employees in the departments to mentor the next generation of Pewlett Hackard employees?
+
